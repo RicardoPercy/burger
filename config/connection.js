60f6735c9fb1d47d54c40fb89112ef776af7ec96
RicardoPercy <ricardopee@gmail.com>
2017-07-11 00:31:04 -0400

    working

--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
+if (process.env.JAWSDB_GOLD_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_GOLD_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "burgers_db"
+    });
+};
 
-
-var connection = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "",
-	database: "burgers_db"
-});
-
-connection.connect(function (err) {
-	if (err) {
-		console.error("error");
-		return;
-	}
-	console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 module.exports = connection;
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var connection = require("./connection");
 var mysql = require("mysql");
 var express = require("express");
-var 
+
+
+
+connection.connect()
